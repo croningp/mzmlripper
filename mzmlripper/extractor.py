57f74315be59f54cd2486168836c65e5df90d81c
Graham Keenan <graham.keenan@glasgow.ac.uk>
2019-07-16 14:27:17 +0100

    Removed dependency on filetools

--- mzmlripper/extractor.py
+++ mzmlripper/extractor.py
@@ -9,12 +9,10 @@ Single processing and bulk processing available.
 
 """
 
-import sys
-import filetools
 from .mzml_parser import MzmlParser
 
 
-def process_single_file(filename: str, out_dir: str, int_threshold=1000):
+def process_mzml_file(filename: str, out_dir: str, int_threshold=1000):
     """Constructs a parser for the mzML file and extracts information
 
     Arguments:
@@ -26,25 +24,3 @@ def process_single_file(filename: str, out_dir: str, int_threshold=1000):
     """
 
     return MzmlParser(filename, out_dir, int_threshold=int_threshold).parse_file()
-
-
-def process_multiple_files(data_folder: str, out_folder: str):
-    """Processes multiple mzML files from a directory
-
-    Arguments:
-        data_folder {str} -- Folder containing all mzML files
-        out_folder {str} -- Directory to store the output
-    """
-
-    for mzmlfile in filetools.list_files(data_folder):
-        if not ".mzML" in mzmlfile:
-            continue
-        process_single_file(mzmlfile, out_folder)
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print(f"MZML folder and output directory required!")
-        sys.exit(-1)
-
-    process_multiple_files(sys.argv[1], sys.argv[2])
