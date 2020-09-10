53982f39bbfad957472e235d202e0bf683f009ef
Graham Keenan <graham.keenan@outlook.com>
2020-09-10 15:52:40 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as fd:
 
 setup(
     name="mzmlripper",
-    version="1.1.0",
+    version="1.1.1",
     author="Graham Keenan",
     author_email="graham.keenan@glasgow.ac.uk",
     description="Extractor for MS1 and MS2 data from mzML file format",
